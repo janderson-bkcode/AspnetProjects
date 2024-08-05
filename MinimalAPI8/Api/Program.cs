ca10a56347810d2488f6e2b140332ee114d95207
Janderson Goncalves <janderson.goncalves@bkbank.com.br>
2024-08-05 16:58:14 -0300

    Add Global Exception Handler

--- MinimalAPI8/Api/Program.cs
+++ MinimalAPI8/Api/Program.cs
@@ -1,3 +1,4 @@
+using Api.Middlewares;
 using Carter;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCarter();
 builder.Services.AddHealthChecks();
+
+//Global Exception Handler
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
+
 builder.Services.AddMediatR(c =>
 {
     c.RegisterServicesFromAssembly(typeof(Program).Assembly);
@@ -21,5 +28,6 @@ if (app.Environment.IsDevelopment())
 app.MapHealthChecks("health");
 app.MapCarter();
 app.UseHttpsRedirection();
+app.UseExceptionHandler();
 app.Run();
 
