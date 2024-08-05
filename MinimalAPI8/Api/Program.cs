a6652bc767fb571fce8b0903768c49663925a9d8
Janderson Goncalves <janderson.goncalves@bkbank.com.br>
2024-08-05 16:12:04 -0300

    add minimalAPiWithCarter

---
+++ MinimalAPI8/Api/Program.cs
@@ -0,0 +1,22 @@
+using Carter;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCarter();
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapHealthChecks("health");
+app.MapCarter();
+app.UseHttpsRedirection();
+
+app.Run();
+
---
+++ MinimalAPI8/Api/WeatherForecastEndPoint.cs
@@ -0,0 +1,33 @@
+﻿using Carter;
+namespace Api;
+
+public class WeatherForecastEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("api/weatherforecast");
+        group.MapGet("", GetForecast);
+    }
+    record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+    private static WeatherForecast[] GetForecast()
+    {
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+
+        return forecast;
+    }
+}
